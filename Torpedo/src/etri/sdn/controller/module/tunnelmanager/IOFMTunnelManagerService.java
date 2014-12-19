6aba1443fe47462e751b4858c2c37dcdb37fd0b3
uni2u <zuno48@gmail.com>
2014-12-19 18:57:47 +0900

    create branch iNaaSAgent

--- Torpedo/src/etri/sdn/controller/module/tunnelmanager/IOFMTunnelManagerService.java
+++
@@ -1,9 +0,0 @@
-package etri.sdn.controller.module.tunnelmanager;
-
-import etri.sdn.controller.IService;
-
-public interface IOFMTunnelManagerService extends IService {
-
-	public void addTunnel(String node_ip_mgt, String node_ip_tun, String node_name, String node_type, String iris_ip);
-	
-}
